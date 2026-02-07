aaeba8d974ec09c3307b25491106254d81fece2e
Subhan Ahmed <subhanahmede860@gmail.com>
2026-02-07 06:31:32 +0500

    Cta Circle Add

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 const lenis = new Lenis({
   autoRaf: true,
 });
+
+
+const ctaButtons = document.querySelectorAll('.cta-circle');
+
+ctaButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        ctaButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+})
